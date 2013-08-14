6b2ab9430c4ef302ed6bfc8eff54d2a581bdf35f
Heromight <RussianDominator@yandex.ru>
2013-08-14 12:40:47 +0600

    Add html/css files to project

---
+++ game/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>	
+	<head>
+		<title> Игра "Пьяница"</title>
+		<meta name="description" content="Free Web tutorials">
+		<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+		<meta name="author" content="Ståle Refsnes">
+		<meta charset="UTF-8">
+		<link rel = "icon" type = "image/x-icon" href = "../images/icon.ico">
+		<link rel = "stylesheet" content = "text/css" href = "main.css">
+	</head>
+	
+	
+	<body>
+		<div id = "all" class = "all">
+			<div id = "workarea" class = "workarea">
+			</div>
+			
+			
+			
+			
+		</div>
+	</body>
+</html>	
